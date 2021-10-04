d7781ea85c43d37222b94984a88f1edcf4ac46a3
MOBUMIN <kiju23@naver.com>
2021-10-04 23:19:33 +0900

    routing 연습

---
+++ components/Header.js
@@ -0,0 +1,14 @@
+function Header() {
+	return(
+		<header>
+			<div>
+				<p>소설이쥬아</p>
+			</div>
+			<div>
+				test
+			</div>
+		</header>
+	)
+}
+
+export default Header
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import styles from '../styles/Home.module.css'
+import Header from '../components/Header';
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       next app!
+	  <Header />
+	  <Link href="/test/about"><a>about.js</a></Link>
     </div>
   )
 }
